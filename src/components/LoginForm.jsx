ad98896eaa3d27ea27333797f3798ee379d9208b
Sahiba Kumari <sahibakumari0915@outlook.com>
2022-10-18 12:25:39 +0530

    update: login

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -8,22 +8,59 @@ const LoginForm = () => {
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
 
-  const handleSubmit = async (e) => {
+  const handelSubmit = async(e) => {
     e.preventDefault();
-
-    const authObject = { 'Project-ID': projectID, 'User-Name': username, 'User-Secret': password, 'Private-Key':'baf5ea61-6af2-4fd3-b393-253dcc60aa07' };
-
-    try {
-      await axios.get('https://api.chatengine.io/users/me', { headers: authObject });
-
-      localStorage.setItem('username', username);
-      localStorage.setItem('password', password);
-
-      window.location.reload();
-      setError('');
-    } catch (err) {
-      setError('Oops, incorrect credentials.');
+    if (!(username.length > 0 && password === confirmPassword && password.length > 0)) return;
+  
+    const authObject = {
+      'Private-Key': '2f389292-d5e1-4799-bd31-b456e7e94845',
+      // 'Project-ID': '25a91c10-8623-4a0d-a48a-3de096d44b54',
+      // 'User-Name': username,
+      // 'User-Secret': password
     }
+  
+    const authHeader = {
+      'Private-Key': '2f389292-d5e1-4799-bd31-b456e7e94845',
+      'Project-ID': '25a91c10-8623-4a0d-a48a-3de096d44b54',
+    }
+    // const authBody = {
+    //    'username': username,
+    //    'secret': password
+    // }
+  
+  
+  
+    try {
+      //fetch the current user if it exists
+      const userExists = await axios.get('https://api.chatengine.io/users', {
+        headers: authObject
+      });
+      console.log('worked');
+      console.log(userExists);
+      // if user already exists and in localStorage redirect them to the chat room
+      if (userExists && (localStorage.getItem('username') && localStorage.getItem('username') === username)) {
+        setTimeout(() => {
+          history.push('/chat')
+        }, 1000)
+        return;
+      }
+      console.log('worked2');
+      // if user don't exists, create a new user 
+      await axios.post('https://api.chatengine.io/users', {
+        headers: authHeader,
+        body: {
+          'username': username,
+          'secret': password
+        }
+      });
+      console.log('worked25');
+      history.push('/login')
+    } catch (error) {
+      console.log(error);
+      setError(`oops, something went wrong`)
+    }
+  
+  }
   };
 
   return (
